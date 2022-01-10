a4d471551c4ecc6077ca40d6162401fa4ade4a8a
xndrbrgs <aborgescolon@gmail.com>
2022-01-10 15:58:28 -0500

    started working the initial html file and added small css

---
+++ Code/script.js
@@ -0,0 +1,17 @@
+// Function to save to local storage
+//Way to pull items from LS
+//Utilize weather API
+//Use info from API to build display
+//
+
+// Global Variables
+
+var searchHistory = [];
+var weatherApiUrl = "https://api.openweathermap.org";
+var weatherApiKey = "1235ceb3341c3c7db44a7f1a34a49d30";
+
+// DOM Elements
+// Main card
+// Seacrh button
+
+// Functions
