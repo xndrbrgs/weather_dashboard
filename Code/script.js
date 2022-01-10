91bacf1d086770ffad05dee09f698c222b3fe9d9
xndrbrgs <aborgescolon@gmail.com>
2022-01-10 18:34:33 -0500

    html mostly done and css as well + starting js

--- Code/script.js
+++ Code/script.js
@@ -11,7 +11,20 @@ var weatherApiUrl = "https://api.openweathermap.org";
 var weatherApiKey = "1235ceb3341c3c7db44a7f1a34a49d30";
 
 // DOM Elements
-// Main card
-// Seacrh button
+var searchButton = $('#searchBtn');
+var clearButton = $('#clearBtn');
+var timeDisplayEl = $('#currentDay');
+var currentTemp = $('#temperature');
+var currentPic = $('#current-picture');
+var currentHumid = $('#humidity');
+var 
 
 // Functions
+
+function displayTime() {
+  var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+setInterval(displayTime, 1000);
+// Event Listeners
