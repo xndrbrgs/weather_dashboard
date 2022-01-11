8babbc54d6a4fe44ebea1b7d83440f01d02f6d29
xndrbrgs <aborgescolon@gmail.com>
2022-01-11 15:52:39 -0500

    fetching weather data from api

--- Code/script.js
+++ Code/script.js
@@ -72,6 +72,62 @@ function renderWeatherData(cityName, cityTemp, cityHumidity, cityWindSpeed, city
   weatherIconEl.attr("src", cityWeatherIcon);
 }
 
+function getWeather(desiredCity) {
+  let queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${desiredCity}&APPID=${weatherApiKey}&units=imperial`;
+  $.ajax({
+      url: queryUrl,
+      method: "GET"
+  })
+  .then(function(weatherData) {
+      let cityObj = {
+          cityName: weatherData.name,
+          cityTemp: weatherData.main.temp,
+          cityHumidity: weatherData.main.humidity,
+          cityWindSpeed: weatherData.wind.speed,
+          cityUVIndex: weatherData.coord,
+          cityWeatherIconName: weatherData.weather[0].icon
+      }
+  let queryUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${cityObj.cityUVIndex.lat}&lon=${cityObj.cityUVIndex.lon}&APPID=${apiKey}&units=imperial`
+  $.ajax({
+      url: queryUrl,
+      method: 'GET'
+  })
+  .then(function(uvData) {
+      if (JSON.parse(localStorage.getItem("searchHistory")) == null) {
+          let searchHistoryArr = [];
+          // Keeps user from adding the same city to the searchHistory array list more than once
+          if (searchHistoryArr.indexOf(cityObj.cityName) === -1) {
+              searchHistoryArr.push(cityObj.cityName);
+              // store our array of searches and save 
+              localStorage.setItem("searchHistory", JSON.stringify(searchHistoryArr));
+              let renderedWeatherIcon = `https:///openweathermap.org/img/w/${cityObj.cityWeatherIconName}.png`;
+              renderWeatherData(cityObj.cityName, cityObj.cityTemp, cityObj.cityHumidity, cityObj.cityWindSpeed, renderedWeatherIcon, uvData.value);
+              renderSearchHistory(cityObj.cityName);
+          }else{
+              console.log("City already in searchHistory. Not adding to history list")
+              let renderedWeatherIcon = `https:///openweathermap.org/img/w/${cityObj.cityWeatherIconName}.png`;
+              renderWeatherData(cityObj.cityName, cityObj.cityTemp, cityObj.cityHumidity, cityObj.cityWindSpeed, renderedWeatherIcon, uvData.value);
+          }
+      }else{
+          let searchHistoryArr = JSON.parse(localStorage.getItem("searchHistory"));
+          // Keeps user from adding the same city to the searchHistory array list more than once
+          if (searchHistoryArr.indexOf(cityObj.cityName) === -1) {
+              searchHistoryArr.push(cityObj.cityName);
+              // store our array of searches and save 
+              localStorage.setItem("searchHistory", JSON.stringify(searchHistoryArr));
+              let renderedWeatherIcon = `https:///openweathermap.org/img/w/${cityObj.cityWeatherIconName}.png`;
+              renderWeatherData(cityObj.cityName, cityObj.cityTemp, cityObj.cityHumidity, cityObj.cityWindSpeed, renderedWeatherIcon, uvData.value);
+              renderSearchHistory(cityObj.cityName);
+          }else{
+              console.log("City already in searchHistory. Not adding to history list")
+              let renderedWeatherIcon = `https:///openweathermap.org/img/w/${cityObj.cityWeatherIconName}.png`;
+              renderWeatherData(cityObj.cityName, cityObj.cityTemp, cityObj.cityHumidity, cityObj.cityWindSpeed, renderedWeatherIcon, uvData.value);
+          }
+      }
+  })
+      
+  });
 
+  
 
 // Event Listeners
